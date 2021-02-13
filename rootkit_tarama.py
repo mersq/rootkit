1be8b351e3c5eb9cd7468f2e28e3a2011b9e4148
mersq <78985433+mersq@users.noreply.github.com>
2021-02-13 12:41:38 +0000

    Add files via upload

---
+++ rootkit_tarama.py
@@ -0,0 +1,13 @@
+#1/home/kali python
+
+import os
+
+os.system("apt-get install figlet")
+os.system("clear")
+os.system("figlet rootkit tarama")
+print("""
+rootkit tarama aracina hos geldin
+
+""")
+os.system("apt-get install chkrootkit")
+os.system("chkrootkit")
